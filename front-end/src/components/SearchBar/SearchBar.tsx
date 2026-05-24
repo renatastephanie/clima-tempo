0cdb233c290cca32d4267d9883efd1f094962c9e
renatastephanie <renatastephaniesantos@gmail.com>
2026-05-24 16:02:53 -0300

    feat: criados os componentes searchBar

---
+++ front-end/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,22 @@
+import styles from "./SearchBar.module.css";
+
+interface SearchBarProps {
+  city: string;
+  onCityChange: (value: string) => void;
+  onSearch: () => void;
+}
+
+export const SearchBar = ({ city, onCityChange, onSearch }: SearchBarProps) => {
+  return (
+    <div className={styles.searchBar}>
+      <input
+        type="text"
+        placeholder="Digite o nome da Cidade..."
+        value={city}
+        onChange={(e) => onCityChange(e.target.value)}
+      />
+
+      <button onChange={onSearch}>Buscar</button>
+    </div>
+  );
+};
